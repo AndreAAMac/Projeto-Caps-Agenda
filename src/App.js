5d4bdca05b4c885633a2404dd47cc2c0ac15e0d7
André Machado <andre_almac@outlook.com>
2024-01-15 21:18:45 -0300

    Projeto Caps Versão 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
 import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+
+  const produto = {
+    codigo:'',
+    nome:'',
+    cidade:'Frutal',
+    situacao:'Primeira vez',
+    dataConsulta:''
+  }
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno=>retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  },[]);
+
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto, [e.target.name]:e.target.value,});
+  }
+
+  //Cadastrar Produto
+  const cadastrar = () =>{
+    fetch("http://localhost:8080/cadastrar",{
+      method:'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      
+      if(retorno_convertido.mensagem !== undefined){
+        alert(retorno_convertido.mensagem);
+      }else{
+        setProdutos([...produtos,retorno_convertido]);
+        alert('Produto cadastrado com sucesso!');
+        limparFormulario();
+      }
+    }
+    )
+  }
+
+  //Limpar formulário
+  const limparFormulario = () =>{
+    setObjProduto(produto);
+    setBtnCadastrar(true);
+  }
+
+  //Selecionar Produto
+  const selecionarProduto = (indice) => {
+    setObjProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
+  //Remover Produto
+  const remover = () =>{
+    fetch("http://localhost:8080/remover/"+objProduto.codigo,{
+      method:'delete',
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      //Mensagem
+      alert(retorno_convertido.mensagem);
+
+      //Cópia de vetor de produtos
+      let vetorTemp = [...produtos];
+
+      //Indice
+      let indice = vetorTemp.findIndex((p)=>{
+        return p.codigo === objProduto.codigo;
+      });
+
+      //Remover produto do vetor temp 
+      vetorTemp.splice(indice, 1);
+
+      //Atualizar vetor de produtos
+      setProdutos(vetorTemp);
+
+      //Limpar formulario
+      limparFormulario();
+
+    })
+  }
+
+  //Alterar Produto
+  const alterar = () =>{
+    fetch("http://localhost:8080/alterar",{
+      method:'put',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      if(retorno_convertido.mensagem !== undefined){
+        alert(retorno_convertido.mensagem);
+      }else{
+        //Mensagem
+        alert('Produto alterado com sucesso!');
+
+        //Cópia de vetor de produtos
+        let vetorTemp = [...produtos];
+
+        //Indice
+        let indice = vetorTemp.findIndex((p)=>{
+          return p.codigo === objProduto.codigo;
+        });
+
+        //Alterar produto do vetor temp 
+        vetorTemp[indice] = objProduto;
+
+        //Atualizar vetor de produtos
+        setProdutos(vetorTemp);
+
+        //Limpar formulario
+        limparFormulario();
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} cancelar={limparFormulario} remover={remover} alterar={alterar}/>
+      <Tabela vetor={produtos} selecionar={selecionarProduto}/>
     </div>
   );
 }
---
+++ src/Formulario.js
@@ -0,0 +1,39 @@
+function Formulario({botao, eventoTeclado, cadastrar, obj, cancelar, remover, alterar})  {
+  return (
+    <form>
+     
+        <input type="text" value={obj.nome} name='nome' onChange={eventoTeclado} placeholder='Nome' className='form-control'  />
+
+        <select value={obj.cidade} name='cidade' placeholder='Cidade' onChange={eventoTeclado} className='form-control' >
+            <option value="Frutal">Frutal</option>
+            <option value="Itapagipe">Itapagipe</option>
+            <option value="Planura">Planura</option>
+            <option value="Pirajuba">Pirajuba</option>
+            <option value="Fronteira">Fronteira</option>
+            <option value="Comendador Gomes">Comendador Gomes</option>
+        </select>
+        
+        <select value={obj.situacao} name="situacao" onChange={eventoTeclado} placeholder='Situação' className='form-control' >
+            <option value="Primeira vez">Primeira vez</option>
+            <option value="Retorno">Retorno</option>
+            <option value="Renovação de Receita">Renovação de Receita</option>
+        </select>
+        
+        <input value={obj.dataConsulta} onChange={eventoTeclado} name='dataConsulta'  placeholder='Data da Consulta' className='form-control' type="date"  />
+
+        {
+            botao
+            ?
+            <input type="button" value='Cadastrar' className='btn btn-primary' onClick={cadastrar}/>
+            :
+            <div>
+                <input type="button" value='Alterar' onClick={alterar} className='btn btn-warning'/>
+                <input type="button" value='Remover' onClick={remover} className='btn btn-danger'/>
+                <input type="button" value='Cancelar' onClick={cancelar} className='btn btn-secondary'/>    
+            </div>
+        }
+    </form>
+  )
+}
+
+export default Formulario;
